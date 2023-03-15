f9ac879e50c4902314e39e9366b0fbc02bd01e2c
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-03-15 14:55:06 +0100

    adding promo section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+
+import Nav from './components/Nav';
 import Header from './components/Header';
+import Promo from './components/Promo';
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <Header firstName="Any name other than Bob" />
       <Header firstName="Jack" />
+      <Promo />
     </div>
   );
 }

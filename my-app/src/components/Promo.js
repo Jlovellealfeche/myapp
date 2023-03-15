aa982a3c2a80dd9aff7098f2a6d69397eebfbbb3
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-03-15 16:09:28 +0100

    making variables and styles in promo.js

--- my-app/src/components/Promo.js
+++ my-app/src/components/Promo.js
@@ -1,11 +1,19 @@
-function Promo() {
+function Promo(props) {
+    const styles = {
+        
+        color: "tomato", 
+        fontSize: "40px",
+    }
+ {/* instead of putting the styles inline you could make it as a variable like this above and declare style property*/} 
     return (
         <div className="promo-selection">
             <div>
-                <h1> Dont't miss this deal!</h1>
+                <h1 style={styles}> 
+                    {props.heading} Don't miss this deal!
+                </h1>
             </div>
             <div>
-                <h2>Subscribe to my newsletter and get all the shop items at 50% off!</h2>
+                <h2> {props.Subheading}Subscribe to my newsletter and get all the shop items at 50% off!</h2>
             </div>
         </div>
     );
